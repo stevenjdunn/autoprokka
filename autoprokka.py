5abf978722c922b2407cd9150402a1806dfc3b68
Steven <mail@stevendunn.co.uk>
2018-08-06 11:34:01 +0100

    Version 1.0
    • Updated to Python 3
        - Tested using 3.5.4
    • Added option to copy .gbk file to output
    • Removed logfile
    • Fixed innocuous extra '/' in directory path

--- autoprokka.py
+++ autoprokka.py
@@ -7,10 +7,14 @@ import argparse
 import sys
 import re
 
+# Version
+_version_="1.0"
+
 # Argparse argument setup
 parser = argparse.ArgumentParser()
 parser.add_argument("-i", "--input", required=True, help="Path to directory containing assemblies to be annotated")
 parser.add_argument("-o", "--output", required=True, help="Path to output destination.")
+parser.add_argument("-gbk", "--genbank", action="store_true", help="Also copies and renames genbank files into output directory.")
 args = parser.parse_args()
 
 # Colour set up
@@ -19,45 +23,32 @@ class colours:
     bold = '\033[1m'
     term = '\033[0m'
 
-# Logger set up
-ansi_rm = re.compile(r'\x1b\[[0-9;]*m')
-class Logger(object):
-    def __init__(self):
-        self.terminal = sys.stdout
-        self.log = open("autospades.log", "w")
-
-    def write(self, message):
-        self.terminal.write(message)
-        self.log.write(re.sub(ansi_rm, '', message))
-
-sys.stdout = Logger()
-
-# Welcome Message# Welcome Message
-print ''
-print ''
-print  colours.bold + '######################'
-print 'Welcome to AutoPROKKA'
-print '######################' + colours.term
-print ''
+# Welcome Message
+print('')
+print('')
+print(colours.bold + '######################')
+print('Welcome to AutoPROKKA')
+print('######################' + colours.term)
+print('')
 
 # Check output path exists, try to write.
 if not os.path.exists(args.output):
     try:
         os.makedirs(args.output)
-        print 'Output directory created.'
-        print ''
+        print('Output directory created.')
+        print('')
     except Exception:
-        print colours.warning + 'Could not create output directory!'
-        print ''
-        print 'Check that the path is correct and that you have the required permissions.'
-        print ''
-        print ''
-        print colours.bold + '#############'
-        print 'Script Failed'
-        print '#############' + colours.term
-        print ''
-        print 'Errors written to:' + os.getcwd() + '/' + 'autoprokka.log'
-        print ''
+        print(colours.warning + 'Could not create output directory!')
+        print('')
+        print('Check that the path is correct and that you have the required permissions.')
+        print('')
+        print('')
+        print(colours.bold + '#############')
+        print('Script Failed')
+        print('#############' + colours.term)
+        print('')
+        print('Errors written to:' + os.getcwd() + '/' + 'autoprokka.log')
+        print('')
         sys.exit(1)
 
 # Directory orientation
@@ -72,39 +63,63 @@ outputgff = outputraw
 # List comprehension
 fasta = list(glob.glob(os.path.join(directory,'*.fasta')))
 output = [x.replace(directory, outputraw).replace('.fasta', '/') for x in fasta]
+copy = [x.replace(directory, outputraw).replace('.fasta','') for x in fasta]
 prefixis = [x.replace(directory, '').replace('.fasta', '') for x in fasta]
 
 #Invoke Prokka
-print ''
-print 'Commencing annotation'
-print ''
+print('')
+print('Commencing annotation')
+print('')
 for fastain, fastaout, pre in zip(fasta, output, prefixis):
         subprocess.call(['prokka', fastain, '-o', fastaout, '--prefix', pre])
-print ''
-print 'Annotation complete!'
-print ''
+print('')
+print('Annotation complete!')
+print('')
 
 # Rename .gff and copy to output.
 gffnames = [x.replace(directory, '').replace('.fasta', '.gff') for x in fasta]
-gfflocations = [''.join(x) for x in zip(output, gffnames)]
+gfflocations = [''.join(x) for x in zip(copy, gffnames)]
 gffoutput = [outputgff + x for x in gffnames]
 for gffin, gffout in zip(gfflocations, gffoutput):
     try:
         shutil.copyfile(gffin, gffout)
     except IOError as e:
-        print colours.warning + 'The following files were not found, indicating that prokka did not finish correctly.'
-        print e
-        print 'Please check FASTA input.' + colours.term
+        print(colours.warning + 'The following files were not found, indicating that prokka did not finish correctly.')
+        print(e)
+        print('Please check FASTA input.' + colours.term)
         sys.exit(1)
+print('')
+print('Copied .gff files.')
+print('')
+
+# Rename .gbk and copy to output.
+if args.genbank:
+    gbknames = [x.replace(directory, '').replace('.fasta', '.gbk') for x in fasta]
+    gbklocations = [''.join(x) for x in zip(copy, gbknames)]
+    gbkoutput = [outputgff + x for x in gbknames]
+    for gbkin, gbkout in zip(gbklocations, gbkoutput):
+        try:
+            shutil.copyfile(gbkin, gbkout)
+        except IOError as e:
+            print(colours.warning)
+            print(e)
+            print('')
+            print('Could not find .gbk files, this is likely due to an outdated tbl2asn binary.')
+            print('')
+            print('Please see here for further details: https://github.com/tseemann/prokka/issues/139')
+            print ('')
+    print('')
+    print('Copied .gbk files.')
+    print('')
+
 # End
-print ''
-print 'Output gff files renamed and stored in:',
-print outputraw
-print ''
-print 'Author: www.github.com/stevenjdunn'
-print ''
-print ''
-print colours.bold +'########'
-print 'Finished'
-print '########' + colours.term
-print ''
+print('')
+print('Output files renamed and stored in:', outputraw)
+print('')
+print('Author: www.github.com/stevenjdunn')
+print('')
+print('')
+print(colours.bold +'########')
+print('Finished')
+print('########' + colours.term)
+print('')
